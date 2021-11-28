54b1526663d2966b321d08f9290116f07bbab8db
caobandoc <obandocarlosandres@gmail.com>
2021-11-28 13:37:23 -0500

    Funciones

--- src/main.go
+++ src/main.go
@@ -2,29 +2,29 @@ package main
 
 import "fmt"
 
-func main() {
-	//Variables
-	helloMessage := "Hello"
-	worldMessage := "world"
-
-	// Println
-	fmt.Println(helloMessage, worldMessage)
-	fmt.Println(helloMessage, worldMessage)
-
-	// Printf
-	nombre := "Carlos"
-	edad := 21
-	//Cuando sabes los tipos de datos
-	fmt.Printf("%s tiene mas de %d años\n", nombre, edad)
-	//Cuando no sabes los tipos de datos
-	fmt.Printf("%v tiene mas de %v años\n", nombre, edad)
-
-	//Sprintf
-	message := fmt.Sprintf("%s tiene mas de %d años", nombre, edad)
+func normalFunction(message string) {
 	fmt.Println(message)
+}
 
-	// Tipo datos
-	fmt.Printf("helloMessage: %T\n", helloMessage)
-	fmt.Printf("edad: %T\n", edad)
+func tripleArgument(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
 
+func returnvalue(a int) int {
+	return a * 2
+}
+
+func doubleReturn(a int) (c, d int) {
+	return a + 1, a * 2
+}
+
+func main() {
+	normalFunction("Hola mundo")
+	tripleArgument(1, 2, "accion")
+
+	value := returnvalue(3)
+	fmt.Println("Value:", value)
+
+	value1, _ := doubleReturn(2)
+	fmt.Println("value1", value1)
 }
