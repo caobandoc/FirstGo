f5617a67ec00a2b2f00be4f353d7e51dbf952c73
caobandoc <obandocarlosandres@gmail.com>
2021-11-29 21:16:32 -0500

    Ciclos For y While

--- src/main.go
+++ src/main.go
@@ -2,29 +2,24 @@ package main
 
 import "fmt"
 
-func normalFunction(message string) {
-	fmt.Println(message)
-}
-
-func tripleArgument(a, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func returnvalue(a int) int {
-	return a * 2
-}
-
-func doubleReturn(a int) (c, d int) {
-	return a + 1, a * 2
-}
-
 func main() {
-	normalFunction("Hola mundo")
-	tripleArgument(1, 2, "accion")
+	// For condicional
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+	fmt.Printf("\n")
+	// For while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
 
-	value := returnvalue(3)
-	fmt.Println("Value:", value)
-
-	value1, _ := doubleReturn(2)
-	fmt.Println("value1", value1)
+	fmt.Printf("\n")
+	//For forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 }
