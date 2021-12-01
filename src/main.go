e849285ab7e888298e5785b26bfd08cc55336187
caobandoc <obandocarlosandres@gmail.com>
2021-11-30 20:36:53 -0500

    defer

--- src/main.go
+++ src/main.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	valor1 := 1
-	//valor2 := 2
-
-	if valor1 == 1 {
-		fmt.Println("Es 1")
-	} else {
-		fmt.Println("No es 1")
-	}
+	// Defer
+	defer fmt.Println("Hola")
+	fmt.Println("Mundo")
 }
