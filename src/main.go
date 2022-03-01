93ecddfd09b20ac7a87430cb017ab9d9d941fa49
caobandoc <obandocarlosandres@gmail.com>
2022-03-01 15:45:57 -0500

    Stringers

--- src/main.go
+++ src/main.go
@@ -8,34 +8,12 @@ type pc struct {
 	brand string
 }
 
-func (myPC pc) ping() {
-	fmt.Println(myPC.brand, "Pong")
-}
-
-func (myPC *pc) duplicateRam() {
-	myPC.ram = myPC.ram * 2
+func (myPC pc) String() string {
+	return fmt.Sprintf("Tengo %d GB RAM, %d GB Disco y es una %s", myPC.ram, myPC.disk, myPC.brand)
 }
 
 func main() {
-	a := 50
-	b := &a
-
-	fmt.Println(b)
-	fmt.Println(*b)
-
-	*b = 100
-	fmt.Println(a)
-
-	myPC := pc{ram: 16, disk: 200, brand: "asus"}
-	fmt.Println(myPC)
-
-	myPC.ping()
-
-	fmt.Println(myPC)
-	myPC.duplicateRam()
-
-	fmt.Println(myPC)
-	myPC.duplicateRam()
+	myPC := pc{ram: 16, brand: "msi", disk: 1000}
 
 	fmt.Println(myPC)
 }
