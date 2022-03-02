055e6482ccdcba9c104e49306c0a0888efb35481
caobandoc <obandocarlosandres@gmail.com>
2022-03-01 20:55:24 -0500

    Go get

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module curso_golang
 
 go 1.17
+
+require (
+	github.com/ant0ine/go-json-rest v3.3.2+incompatible // indirect
+	github.com/yuin/goldmark v1.4.7 // indirect
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	golang.org/x/tools v0.1.9 // indirect
+	golang.org/x/tour v0.1.0 // indirect
+	golang.org/x/website/tour v0.0.0-20210616181959-e0d934b43647 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,38 +1,19 @@
 package main
 
-import "fmt"
+// Libreria a importar: go get github.com/ant0ine/go-json-rest/rest
 
-func message(text string, c chan<- string) {
-	c <- text
-}
+import (
+	"log"
+	"net/http"
+
+	"github.com/ant0ine/go-json-rest/rest"
+)
 
 func main() {
-	c := make(chan string, 2)
-	c <- "Mensaje1"
-	c <- "Mensaje2"
-
-	fmt.Println(len(c), cap(c))
-
-	// Range y close
-	close(c)
-
-	// c <- "Mensaje3"
-
-	for message := range c {
-		fmt.Println(message)
-	}
-
-	// Select
-	email1 := make(chan string)
-	email2 := make(chan string)
-	go message("mensaje1", email1)
-	go message("mensaje2", email2)
-	for i := 0; i < 2; i++ {
-		select {
-		case m1 := <-email1:
-			fmt.Println("Email recibido de email1", m1)
-		case m2 := <-email2:
-			fmt.Println("Email recibido de email2", m2)
-		}
-	}
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request) {
+		w.WriteJson(map[string]string{"Body": "Hello World!"})
+	}))
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
 }
